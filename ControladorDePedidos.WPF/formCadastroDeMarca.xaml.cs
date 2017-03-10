35b6d5b7883187bef55a622147f39e2c4c890eef
felip <felip@DESKTOP-L1O8TQ8>
2017-03-10 13:31:08 -0300

    Adicionado o projeto

---
+++ ControladorDePedidos.WPF/formCadastroDeMarca.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ControladorDePedidos.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControladorDePedidos.WPF
+{
+    /// <summary>
+    /// Interaction logic for formCadastroDeMarca.xaml
+    /// </summary>
+    public partial class formCadastroDeMarca : Window
+    {
+        public formCadastroDeMarca(Marca marca)
+        {
+            InitializeComponent();
+            txtCodigo.Text = marca.Codigo.ToString();
+            txtNome.Text = marca.nome;
+
+
+        }
+
+        public formCadastroDeMarca()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            var codigo = txtCodigo.Text;
+            var Nome = txtNome.Text;
+
+            if (codigo == "")
+            {
+                //Novo cadastro
+                var marca = new Marca
+                {
+
+                    nome = Nome
+                };
+                //Cadastrar no banco de dados!!
+            }
+            else
+            {
+                //Editando
+                var marca = new Marca
+                {
+                    Codigo = int.Parse(codigo),
+                    nome = Nome
+                };
+            }
+
+            //Atualizar no banco de dados!!
+        }
+    }
+}
---
+++ ControladorDePedidos.WPF/formMarcas.xaml.cs
@@ -0,0 +1,69 @@
+﻿using ControladorDePedidos.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControladorDePedidos.WPF
+{
+    /// <summary>
+    /// Interaction logic for formMarcas.xaml
+    /// </summary>
+    public partial class formMarcas : Window
+    {
+        public formMarcas()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var marcas = new List<Marca>();
+
+            var marca1 = new Marca
+            {
+                Codigo = 1,
+                nome = "CCE"
+
+            };
+            var marca2 = new Marca
+            {
+                Codigo = 2,
+                nome = "Dell"
+            };
+            marcas.Add(marca1);
+            marcas.Add(marca2);
+            lstMarcas.DataContext = marcas;
+        }
+
+        private void btnNovo_Click(object sender, RoutedEventArgs e)
+        {
+            var formCadastroMarcas = new formCadastroDeMarca();
+            formCadastroMarcas.Show();
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstMarcas.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione um item");
+              
+            }
+            else
+            {
+                var itemSelecionado = (Marca)lstMarcas.SelectedItem;
+                var formCadastroMarcas = new formCadastroDeMarca(itemSelecionado);
+                formCadastroMarcas.Show();
+            }
+        }
+    }
+}
